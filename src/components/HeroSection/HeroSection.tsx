005b51e5ff24178f05bcf40bb7b773f70cd9a095
yhonaiker moncada <yhonaiker1@gmail.com>
2025-04-21 22:51:18 -0500

    feat: added hero section with responsive design and call-to-action buttons

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,45 +4,52 @@ import { Button } from '../ui/StyledComponents'
 import { HeroImage, HeroText, HeroWrapper } from './HeroSection.style'
 import { ActionButtons } from './HeroSection.style'
 import { HeroContent } from './HeroSection.style'
+import Image from 'next/image'
+import heroMobile from '../../../public/images/digitalBank.avif'
 
 const HeroSection: React.FC = () => {
+  const handleNavLinkClick = (
+    e: React.MouseEvent<HTMLElement>,
+    href: string
+  ) => {
+    e.preventDefault()
+
+    const targetId = href.replace('#', '')
+    const targetElement = document.getElementById(targetId)
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
   return (
     <HeroWrapper>
       <Container>
         <HeroContent>
           <HeroText>
-            <h2>Banking that puts you in control</h2>
+            <h2>Banca que te pone en control</h2>
             <p>
-              Modern, secure and accessible financial products designed for your
-              lifestyle. Manage your money with confidence and enjoy
-              personalized solutions that fit your needs.
+              Productos financieros modernos, seguros y accesibles diseñados
+              para tu estilo de vida. Administra tu dinero con confianza y
+              disfruta de soluciones personalizadas que se ajustan a tus
+              necesidades.
             </p>
             <ActionButtons>
               <Button
-                className="primary"
-                tabIndex={0}
-                aria-label="Open an account"
-              >
-                Open an account
-              </Button>
-              <Button
                 className="secondary"
                 tabIndex={0}
-                aria-label="Explore our products"
+                aria-label="Explorar nuestros productos"
+                onClick={(e) => handleNavLinkClick(e, '#products')}
               >
-                Explore products
+                Explorar productos
               </Button>
             </ActionButtons>
           </HeroText>
           <HeroImage>
             <picture>
-              <source
-                media="(min-width: 768px)"
-                srcSet="/images/products/hero-desktop.jpg"
-              />
-              <img
-                src="/images/products/hero-mobile.jpg"
-                alt="Person using DigitalBank mobile app"
+              <source media="(min-width: 768px)" srcSet={heroMobile.src} />
+              <Image
+                src={heroMobile}
+                alt="Persona usando la aplicación móvil de DigitalBank"
                 width={500}
                 height={300}
               />
